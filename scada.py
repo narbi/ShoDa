59c40b73b5ca2ae97ea69a4503092703ad543348
Christina <skouloudi@gmail.com>
2017-04-27 01:51:30 +0300

    count top protocols for the four levels
    TODO: get protocol version in each case

---
+++ scada.py
@@ -0,0 +1,40 @@
+import shodan
+
+SHODAN_API_KEY = "T9NuuJ24Fhj4UC8hZ94w8EtSv4f7XkWg"
+api = shodan.Shodan(SHODAN_API_KEY)
+
+# Top SCADA Protocols
+protocols = ['MODBUS', 'DNP3', 'ISC 61850', 'FTP'];
+
+# Create facets
+FACETS = [
+    'org',
+    'domain',
+    'port',
+    'asn',
+    ('country', 20),
+]
+
+FACET_TITLES = {
+    'org': 'Top 10 Organizations',
+    'domain': 'Top 10 Domains',
+    'port': 'Top 10 Ports',
+    'asn': 'Top 10 Autonomous Systems',
+    'country': 'Top 20 Countries',
+}
+
+
+for protocol in protocols:
+  try:
+    # Search Shodan
+    query= protocol
+    results = api.count(query, facets=FACETS)
+    # Show the results
+    print 'Total Results for %s: %s \n' % (protocol, results['total'])
+    for result in results['matches']:
+      print FACET_TITLES[facet]
+    for term in result['facets'][facet]:
+      print '%s: %s' % (term['value'], term['count'])
+    print ''
+  except shodan.APIError, e:
+    print 'Error: %s' % e
