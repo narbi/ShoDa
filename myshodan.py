fefc34da7f453be3655fd2e4cb45190425cafee2
Christina Skouloudi <skouloudi@gmail.com>
2017-08-03 10:37:24 +0300

    supplied with logic: from generic to specific

--- myshodan.py
+++ myshodan.py
@@ -6,37 +6,38 @@ SHODAN_API_KEY = "T9NuuJ24Fhj4UC8hZ94w8EtSv4f7XkWg"
 api = shodan.Shodan(SHODAN_API_KEY)
 
 # Top SCADA Protocols
-protocols = ['MODBUS', 'DNP3', 'ISC 61850', 'FTP'];
+# protocols = ['ICS', 'SCADA', 'MODBUS', 'DNP3', 'IEC 61850', 'SCADA FTP'];
+protocols = ['ICS', 'SCADA', 'MODBUS', 'MODBUS TCP/IP'];
 
 # Create facets
 FACETS = [
-    'org',
-    'domain',
     'port',
-    'asn',
-    ('country', 20),
+    'os',
+    'device',
+    'version',
+    ('country', 10),
 ]
 
 FACET_TITLES = {
-    'org': 'Top 10 Organizations',
-    'domain': 'Top 10 Domains',
-    'port': 'Top 10 Ports',
-    'asn': 'Top 10 Autonomous Systems',
-    'country': 'Top 20 Countries',
+    'port': 'Top 5 Ports',
+    'os': 'Top 5 Operating Systems',
+    'device': 'Top 5 Devices',
+    'version': 'Top 5 Product Versions',
+    'country': 'Top 10 Countries'
 }
-
 
 for protocol in protocols:
   try:
     # Search Shodan
-    query= protocol
+    query = protocol
     results = api.count(query, facets=FACETS)
     # Show the results
     print ('Total Results for %s: %s \n' % (protocol, results['total']))
-    for result in results['matches']:
-      print (FACET_TITLES[facet])
-    for term in result['facets'][facet]:
-      print ('%s: %s' % (term['value'], term['count']))
+    for facet in results['facets']:
+        print ('\n')
+        print (FACET_TITLES[facet])
+        for term in results['facets'][facet]:
+            print ('%s: %s' % (term['value'], term['count']))
     print ('-------------------------------------')
   except shodan.APIError:
     print ('Error: %s' % e)
